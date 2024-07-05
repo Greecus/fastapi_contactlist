6994f96e7ba3d8abd7a144b6cc9379b870cbe36b
Krzysztof Jaszewski <KrzysztofJasz2002@gmail.com>
2024-07-06 00:53:44 +0200

    added authentication

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean, func, Table, Date
+from sqlalchemy import Column, Integer, String, func, Date, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,3 +14,16 @@ class Contact(Base):
     phone = Column(String(12), nullable=False)
     birthday = Column(Date, nullable=False)
     addition_info = Column(String(100), nullable=True)
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="notes")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -3,34 +3,34 @@ from typing import List
 from sqlalchemy import or_, and_, extract
 from sqlalchemy.orm import Session
 
-from src.database.models import Contact
+from src.database.models import Contact, User
 from src.schemas import ContactModel, ContactUpdate
 
 from datetime import date, timedelta
 
-async def create_contact(body: ContactModel, db: Session) -> Contact:
-    contact = Contact(name=body.name, surname=body.surname, email=body.email, phone=body.phone, birthday=body.birthday, addition_info=body.addition_info)
+async def create_contact(body: ContactModel, user: User, db: Session) -> Contact:
+    contact = Contact(name=body.name, surname=body.surname, email=body.email, phone=body.phone, birthday=body.birthday, user=user, addition_info=body.addition_info)
     db.add(contact)
     db.commit()
     db.refresh(contact)
     return contact
 
-async def get_contacts(skip: int, limit: int, db: Session) -> List[Contact]:
-    return db.query(Contact).offset(skip).limit(limit).all()
+async def get_contacts(skip: int, limit: int, user: User, db: Session) -> List[Contact]:
+    return db.query(Contact).filter(Contact.user_id == user.id).offset(skip).limit(limit).all()
 
-async def get_contact(id: int, db: Session) -> Contact:
-    return db.query(Contact).filter(Contact.id == id).first()
+async def get_contact(id: int, user: User, db: Session) -> Contact:
+    return db.query(Contact).filter(and_(Contact.id == id,Contact.user_id == user.id)).first()
 
-async def search_contacts(match_str: str, db: Session) -> List[Contact] | None:
+async def search_contacts(match_str: str, user: User, db: Session) -> List[Contact] | None:
     matching_contacts = list() 
-    matching_contacts.extend(db.query(Contact).filter(Contact.name == match_str).all())
-    matching_contacts.extend(db.query(Contact).filter(Contact.surname == match_str).all())
-    matching_contacts.extend(db.query(Contact).filter(Contact.email == match_str).all())
+    matching_contacts.extend(db.query(Contact).filter(and_(Contact.name == match_str,Contact.user_id == user.id)).all())
+    matching_contacts.extend(db.query(Contact).filter(and_(Contact.surname == match_str,Contact.user_id == user.id)).all())
+    matching_contacts.extend(db.query(Contact).filter(and_(Contact.email == match_str,Contact.user_id == user.id)).all())
     if not matching_contacts: return None
     return matching_contacts
 
-async def update_contact(contact_id: int, body: ContactUpdate, db: Session) -> Contact | None:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def update_contact(contact_id: int, body: ContactUpdate, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id,Contact.user_id == user.id)).first()
     if contact:
         contact.name = body.name
         contact.surname = body.surname
@@ -41,18 +41,18 @@ async def update_contact(contact_id: int, body: ContactUpdate, db: Session) -> C
         db.commit()
     return contact
 
-async def remove_contact(contact_id: int, db: Session) -> Contact | None:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def remove_contact(contact_id: int, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id,Contact.user_id == user.id)).first()
     if contact:
         db.delete(contact)
         db.commit()
     return contact
 
-async def get_close_birthdays(limit:int, db: Session) -> List[Contact]:
+async def get_close_birthdays(limit:int, user: User, db: Session) -> List[Contact]:
     today = date.today()
     end_day = date.today() + timedelta(days=limit)
-    return db.query(Contact).filter(or_(
+    return db.query(Contact).filter(and_(or_(
         and_(extract('month', Contact.birthday) == today.month,
              extract('day', Contact.birthday) >= today.day),
         and_(extract('month', Contact.birthday) == end_day.month,
-             extract('day', Contact.birthday) <= end_day.day))).all()
+             extract('day', Contact.birthday) <= end_day.day)),Contact.user_id == user.id)).all()
